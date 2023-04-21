1e0703c9a71a98099fa03693336dc7bd7a3946c5
fcardenas-at-086747970945 <fcardenasmena@santandertecnologia.com.ar>
2023-04-21 17:44:59 -0300

    Stretch Challenge prep 2 ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -33,7 +33,34 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your letter grade is: {letter}");
+        string sign = "";
+
+        if (grade % 10 >= 7)
+        {
+            sign = "+";
+        } 
+        else if (grade % 10 <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Print letter grade
+        if (letter == "A" && sign == "+")
+        {
+            Console.WriteLine($"Your letter grade is: {letter}");
+        }
+        else if (letter == "F" && sign == "+" || sign == "-")
+        {
+            Console.WriteLine($"Your letter grade is: {letter}");
+        }
+        else 
+        {
+            Console.WriteLine($"Your letter grade is: {letter}{sign}");
+        }
 
         if (grade >= 70)
         {
