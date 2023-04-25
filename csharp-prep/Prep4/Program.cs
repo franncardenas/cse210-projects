040356cb72fb89072525fec17ba49c8dfa05c850
fcardenas-at-086747970945 <fcardenasmena@santandertecnologia.com.ar>
2023-04-25 14:35:39 -0300

    W2-Prep4 Ready

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1; 
+        while (userNumber != 0)
+        {
+            
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+                
+        }
+        float sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+        
+
+        int minNumber = 9999999;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && minNumber > number)
+            {
+                minNumber = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {minNumber}");
+
+        numbers.Sort();
+        
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        
     }
 }
