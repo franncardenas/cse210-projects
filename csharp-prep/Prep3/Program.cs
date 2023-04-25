5faa930d1b5ca8d042e53ef70bd806ebde12fda8
fcardenas-at-086747970945 <fcardenasmena@santandertecnologia.com.ar>
2023-04-25 14:34:04 -0300

    w2-Prep3 Ready

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Get number from the user
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (magicNumber != guess)
+        {
+            // Get guess from the user
+            Console.Write("What is your guess? ");
+            string guessUser = Console.ReadLine();
+            guess = int.Parse(guessUser);
+
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
