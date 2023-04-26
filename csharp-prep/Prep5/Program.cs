b362f4ece944f3acdb5f1836c9c59abeeb87965f
fcardenas-at-086747970945 <fcardenasmena@santandertecnologia.com.ar>
2023-04-25 22:11:06 -0300

    w2-Prep5 Ready

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+
+        return square;
+    }
+
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.Write($"{userName}, the square of your number is {squareNumber}");
+    } 
+
+
 }
