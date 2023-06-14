10036ce0bc911e66744da2d2c96e2ca11ef2a672
fcardenas-at-086747970945 <fcardenasmena@santandertecnologia.com.ar>
2023-06-14 17:36:32 -0300

    W9 Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 4);
+        // string color = square.GetColor();
+        // double area = square.GetArea();
+
+        // Console.WriteLine($"The color is: {color}");
+        // Console.WriteLine($"The area is: {area}");
+
+        Rectangle rectangle = new Rectangle("Yellow", 6,4);
+        // string colorr = rectangle.GetColor();
+        // double arear = rectangle.GetArea();
+
+        // Console.WriteLine($"The color is: {colorr}");
+        // Console.WriteLine($"The area is: {arear}");
+
+        Circle circle = new Circle("Pink", 12);
+        // string colorc = circle.GetColor();
+        // double areac = circle.GetArea();
+
+        // Console.WriteLine($"The color is: {colorc}");
+        // Console.WriteLine($"The area is: {areac}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape sh in shapes)
+        {
+            string color = sh.GetColor();
+            double area = sh.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
